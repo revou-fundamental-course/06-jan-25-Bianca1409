fbc853a85841da7faa71e80bbd495b43ffb57411
Bianca <bianca.chrysilla@gmail.com>
2025-01-16 22:16:43 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 /*Ini File Java Script*/
+
+
+function validateForm() {
+    const nama = document.forms ['messege-form']['name-input'].value;
+
+    if(nama==""){
+        document.getElementById("error-name").innerHTML="Nama tidak  boleh kosong"
+
+        return false
+     }
+
+     setName (nama)
+
+     return false;
+    }
+
+
+   
